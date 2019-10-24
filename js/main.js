9f2de423bd14431a6079fe404d28fbef095fc21f
dgaleanoservian-guest <dgaleanoservian@gmail.com>
2019-10-24 17:59:31 +0200

    add mapbox-token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiZGVieWdhbHNlciIsImEiOiJjazI0dnhpdWcwbmVjM2pxbWFkbHRtYXF1In0.3pFLcvP4Yu5MOakH1fH2mQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
