1120a594be4ae758e663d5e35fae36e3d502d52c
dgaleanoservian-guest <dgaleanoservian@gmail.com>
2019-10-29 22:11:54 +0100

    fix offline mode

--- js/main.js
+++ js/main.js
@@ -72,20 +72,27 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
  * Initialize leaflet map, called from HTML.
  */
 initMap = () => {
-  self.newMap = L.map('map', {
+  // if online, then 'try' to load the map
+  // if not, then 'catch' an error and log it 
+  if(navigator.onLine) {
+    try {
+      self.newMap = L.map('map', {
         center: [40.722216, -73.987501],
         zoom: 12,
         scrollWheelZoom: false
       });
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiZGVieWdhbHNlciIsImEiOiJjazI0dnhpdWcwbmVjM2pxbWFkbHRtYXF1In0.3pFLcvP4Yu5MOakH1fH2mQ',
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-  }).addTo(newMap);
-
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1IjoiZGVieWdhbHNlciIsImEiOiJjazI0dnhpdWcwbmVjM2pxbWFkbHRtYXF1In0.3pFLcvP4Yu5MOakH1fH2mQ',
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+      }).addTo(newMap);
+    } catch(error) {
+      console.log("Map couldn't be loaded", error); 
+    }
+  }
   updateRestaurants();
 }
 /* window.initMap = () => {
@@ -188,6 +195,8 @@ createRestaurantHTML = (restaurant) => {
  * Add markers for current restaurants to the map.
  */
 addMarkersToMap = (restaurants = self.restaurants) => {
+  // if no newMap or L - leaflet are not defined, then exit early. 
+  if (!newMap || !L) return;
   restaurants.forEach(restaurant => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
@@ -197,7 +206,6 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     }
     self.markers.push(marker);
   });
-
 } 
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
