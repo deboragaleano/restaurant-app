edcf53d91357a8c02b38bc6b9b698d33fc3e7057
dgaleanoservian-guest <dgaleanoservian@gmail.com>
2019-10-29 21:19:43 +0100

    add fetch requests

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
 const appName = 'restaurant-app'; 
 const staticCacheName = appName + '-v1.0'; 
 const imgsCache = appName + '-images'; 
-const allCaches = [staticCacheName, imgsCache]; 
+let allCaches = [staticCacheName, imgsCache]; 
 
 
-// install SW, cache all statis assets
+// When installing SW, cache all statics assets for offline
 self.addEventListener('install', event => 
     event.waitUntil(
         caches.open(staticCacheName).then( cache => {
             return cache.addAll([
-                '/',
+                '/', // caches index.html 
+                '/restaurant.html', 
                 'css/styles.css', 
                 'js/dbhelper.js', 
                 'js/main.js', 
@@ -22,6 +23,7 @@ self.addEventListener('install', event =>
 );
 
 // Delete old static cache if name doesn't match the current version of the app
+// this is to ensure we have the latest version 
 self.addEventListener('activate', event =>
     event.waitUntil(
         caches.keys().then( cachesNames => {
@@ -37,3 +39,13 @@ self.addEventListener('activate', event =>
     )
 )
 
+//We need to 'fetch' to get things out of the cache
+//If the request got a match in the cache, I’ll return it 
+//Otherwise, I’ll return a fetch to the network for the original request 
+self.addEventListener('fetch', event =>
+    event.respondWith(
+        caches.match(event.request).then( response => {
+            return response || fetch(event.request); 
+        })
+    )
+)
